3e913f68ab74ff2dfc0a1d1b4de8ac43ae2bddfd
azuchi <azuchi@haw.co.jp>
2017-06-16 10:31:33 +0900

    Add Bitcoin#chain_params

--- lib/bitcoin.rb
+++ lib/bitcoin.rb
@@ -5,6 +5,29 @@ module Bitcoin
   autoload :ChainParams, 'bitcoin/chain_params'
   autoload :Logger, 'bitcoin/logger'
 
+  @chain_param = :mainnet
+
+  # set bitcoin network chain params
+  def self.chain_params=(name)
+    raise "chain params for #{name} is not defined." unless %i(mainnet testnet regtest).include?(name.to_sym)
+    @current_chain = nil
+    @chain_param = name.to_sym
+  end
+
+  # current bitcoin network chain params.
+  def self.chain_params
+    return @current_chain if @current_chain
+    case @chain_param
+      when :mainnet
+        Bitcoin::ChainParams.mainnet
+      when :testnet
+        Bitcoin::ChainParams.testnet
+      when :regtest
+        Bitcoin::ChainParams.regtest
+    end
+  end
+
+  # base dir path that store blockchain data and wallet data
   def self.base_dir
     "#{Dir.home}/.bitcoinrb"
   end
--- spec/bitcoin_spec.rb
+++ spec/bitcoin_spec.rb
@@ -1,7 +1,19 @@
 require 'spec_helper'
 
 describe Bitcoin do
+
   it 'has a version number' do
     expect(Bitcoin::VERSION).not_to be nil
   end
+
+  it 'change bitcoin network' do
+    Bitcoin.chain_params = :mainnet
+    expect(Bitcoin.chain_params.network).to eq('mainnet')
+    Bitcoin.chain_params = :testnet
+    expect(Bitcoin.chain_params.network).to eq('testnet')
+    Bitcoin.chain_params = :regtest
+    expect(Bitcoin.chain_params.network).to eq('regtest')
+    expect { Bitcoin.chain_params = :hoge }.to raise_error(RuntimeError)
+  end
+
 end
