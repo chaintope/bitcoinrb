f38a0a4743d9790a94fad50f8b6ce6f2e142e97e
azuchi <azuchi@haw.co.jp>
2017-07-18 22:40:12 +0900

    Support OP_GREATERTHAN in Bitcoin::ScriptInterpreter

--- lib/bitcoin/script/script_interpreter.rb
+++ lib/bitcoin/script/script_interpreter.rb
@@ -220,6 +220,10 @@ module Bitcoin
                   return set_error(ScriptError::SCRIPT_ERR_INVALID_STACK_OPERATION) if stack.size < 2
                   a, b = pop_int(2)
                   stack << (a < b ? 1 : 0)
+                when OP_GREATERTHAN
+                  return set_error(ScriptError::SCRIPT_ERR_INVALID_STACK_OPERATION) if stack.size < 2
+                  a, b = pop_int(2)
+                  stack << (a > b ? 1 : 0)
                 when OP_NOT
                   return set_error(ScriptError::SCRIPT_ERR_INVALID_STACK_OPERATION) if stack.size < 1
                   stack << (pop_int == 0 ? 1 : 0)
