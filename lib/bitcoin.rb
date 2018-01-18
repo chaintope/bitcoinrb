071769def6969e4b8c44e26c221c77486aed10e9
Hajime Yamaguchi <h_yamaguchi@haw.co.jp>
2018-01-18 19:34:48 +0900

    Improve Object#build_json. String in array should work properly.

--- lib/bitcoin.rb
+++ lib/bitcoin.rb
@@ -145,6 +145,7 @@ module Bitcoin
     end
 
     def to_h
+      return self if self.is_a?(String)
       instance_variables.inject({}) do |result, var|
         key = var.to_s
         key.slice!(0) if key.start_with?('@')
