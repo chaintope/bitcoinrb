911cc9a76dc0f9c4a20b1c3c46757b1b9b89dd2e
azuchi <azuchi@haw.co.jp>
2018-02-04 16:21:43 +0900

    Add calculation of short transaction id used in Compact Block

--- bitcoinrb.gemspec
+++ bitcoinrb.gemspec
@@ -32,6 +32,7 @@ Gem::Specification.new do |spec|
   spec.add_runtime_dependency 'eventmachine_httpserver'
   spec.add_runtime_dependency 'rest-client'
   spec.add_runtime_dependency 'iniparse'
+  spec.add_runtime_dependency 'siphash'
 
   spec.add_development_dependency 'bundler', '~> 1.11'
   spec.add_development_dependency 'rake', '~> 10.0'
--- lib/bitcoin/message/header_and_short_ids.rb
+++ lib/bitcoin/message/header_and_short_ids.rb
@@ -1,41 +1,56 @@
+require 'siphash'
 module Bitcoin
   module Message
+
+    # BIP-152 Compact Block's data format.
+    # https://github.com/bitcoin/bips/blob/master/bip-0152.mediawiki#HeaderAndShortIDs
     class HeaderAndShortIDs
 
       attr_accessor :header
       attr_accessor :nonce
-      attr_accessor :shortids
-      attr_accessor :prefilledtxn
+      attr_accessor :short_ids
+      attr_accessor :prefilled_txn
+      attr_accessor :siphash_key
 
-      def initialize
-        @shortids = []
-        @prefilledtxn = []
+      def initialize(header, nonce, short_ids, prefilled_txn)
+        @header = header
+        @nonce = nonce
+        @short_ids = short_ids
+        @prefilled_txn = prefilled_txn
+        @siphash_key = Bitcoin.sha256(header.to_payload << [nonce].pack('q*'))[0...16]
       end
 
       def self.parse_from_payload(payload)
         buf = StringIO.new(payload)
-        ids = self.new
-        ids.header = Bitcoin::BlockHeader.parse_from_payload(buf.read(80))
-        ids.nonce = buf.read(8).unpack('q*').first
-        shortids_len = Bitcoin.unpack_var_int_from_io(buf)
-        shortids_len.times do
-          ids.shortids << buf.read(6).reverse.bth.to_i(16)
+        header = Bitcoin::BlockHeader.parse_from_payload(buf.read(80))
+        nonce = buf.read(8).unpack('q*').first
+        short_ids_len = Bitcoin.unpack_var_int_from_io(buf)
+        short_ids = short_ids_len.times.map do
+           buf.read(6).reverse.bth.to_i(16)
         end
-        prefilledtxn_len = Bitcoin.unpack_var_int_from_io(buf)
-        prefilledtxn_len.times do
-          ids.prefilledtxn << PrefilledTx.parse_from_io(buf)
+        prefilled_txn_len = Bitcoin.unpack_var_int_from_io(buf)
+        prefilled_txn = prefilled_txn_len.times.map do
+          PrefilledTx.parse_from_io(buf)
         end
-        ids
+        self.new(header, nonce, short_ids, prefilled_txn)
       end
 
       def to_payload
         p = header.to_payload
         p << [nonce].pack('q*')
-        p << Bitcoin.pack_var_int(shortids.size)
-        p << shortids.map{|id|sprintf('%12x', id).htb.reverse}.join
-        p << Bitcoin.pack_var_int(prefilledtxn.size)
-        p << prefilledtxn.map(&:to_payload).join
+        p << Bitcoin.pack_var_int(short_ids.size)
+        p << short_ids.map{|id|sprintf('%12x', id).htb.reverse}.join
+        p << Bitcoin.pack_var_int(prefilled_txn.size)
+        p << prefilled_txn.map(&:to_payload).join
         p
+      end
+
+      # calculate short transaction id which specified by BIP-152.
+      # @param [String] txid a transaction id
+      # @return [Integer] 6 bytes short transaction id.
+      def short_id(txid)
+        hash = SipHash.digest(siphash_key, txid.htb.reverse)
+        [hash.to_s(16)].pack('H*')[2...8].bth.to_i(16)
       end
 
     end
