5325214b31ff0d140d78fe59d102bae32c393e76
azuchi <azuchi@haw.co.jp>
2018-08-02 15:32:40 +0900

    Add OpenAssets::MarkerOutput#oa_payload which return OA payload

--- lib/openassets/marker_output.rb
+++ lib/openassets/marker_output.rb
@@ -5,8 +5,14 @@ module OpenAssets
     # whether this output is marker output for open assets.
     def open_assets_marker?
       return false unless script_pubkey.op_return?
-      payload = Payload.parse_from_payload(script_pubkey.op_return_data)
-      !payload.nil?
+      !oa_payload.nil?
+    end
+
+    # get open asset payload.
+    # @return [OpenAssets::Payload] open asset payload.
+    def oa_payload
+      return nil unless script_pubkey.op_return?
+      Payload.parse_from_payload(script_pubkey.op_return_data)
     end
 
   end
