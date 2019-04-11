92d3c8a1eefb60ecb53490f21614eeecf631cc54
azuchi <azuchi@chaintope.com>
2019-04-11 09:35:49 +0900

    Change variable to constants in rfc6979 nonce derivation

--- lib/bitcoin/secp256k1/ruby.rb
+++ lib/bitcoin/secp256k1/ruby.rb
@@ -87,17 +87,22 @@ module Bitcoin
         end
       end
 
+      INITIAL_V = '0101010101010101010101010101010101010101010101010101010101010101'.htb
+      INITIAL_K = '0000000000000000000000000000000000000000000000000000000000000000'.htb
+      ZERO_B = '00'.htb
+      ONE_B = '01'.htb
+
       # generate temporary key k to be used when ECDSA sign.
       # https://tools.ietf.org/html/rfc6979#section-3.2
       def generate_rfc6979_nonce(data, privkey, extra_entropy)
-        v = ('01' * 32).htb # 3.2.b
-        k = ('00' * 32).htb # 3.2.c
+        v = INITIAL_V # 3.2.b
+        k = INITIAL_K # 3.2.c
         # 3.2.d
-        k = Bitcoin.hmac_sha256(k, v + '00'.htb + privkey + data + extra_entropy)
+        k = Bitcoin.hmac_sha256(k, v + ZERO_B + privkey + data + extra_entropy)
         # 3.2.e
         v = Bitcoin.hmac_sha256(k, v)
         # 3.2.f
-        k = Bitcoin.hmac_sha256(k, v + '01'.htb + privkey + data + extra_entropy)
+        k = Bitcoin.hmac_sha256(k, v + ONE_B + privkey + data + extra_entropy)
         # 3.2.g
         v = Bitcoin.hmac_sha256(k, v)
         # 3.2.h
