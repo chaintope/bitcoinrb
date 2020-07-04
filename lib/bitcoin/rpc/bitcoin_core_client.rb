65c0f939ea3f1e825513d88ae1e267d3e315ca28
azuchi <azuchi@chaintope.com>
2020-07-04 14:50:59 +0900

    Remove rest-client dependency from BitcoinCoreClient

--- lib/bitcoin/rpc/bitcoin_core_client.rb
+++ lib/bitcoin/rpc/bitcoin_core_client.rb
@@ -1,4 +1,4 @@
-require 'rest-client'
+require 'net/http'
 
 module Bitcoin
   module RPC
@@ -53,17 +53,18 @@ module Bitcoin
             :params => params,
             :id => 'jsonrpc'
         }
-        post(server_url, @config[:timeout], @config[:open_timeout], data.to_json, content_type: :json) do |respdata, request, result|
-          raise result.message if !result.kind_of?(Net::HTTPSuccess) && respdata.empty?
-          response = JSON.parse(respdata.gsub(/\\u([\da-fA-F]{4})/) { [$1].pack('H*').unpack('n*').pack('U*').encode('ISO-8859-1').force_encoding('UTF-8') })
-          raise response['error'].to_s if response['error']
-          response['result']
-        end
-      end
-
-      def post(url, timeout, open_timeout, payload, headers={}, &block)
-        RestClient::Request.execute(method: :post, url: url, timeout: timeout,
-                                    open_timeout: open_timeout, payload: payload, headers: headers, &block)
+        uri = URI.parse(server_url)
+        http = Net::HTTP.new(uri.hostname, uri.port)
+        http.use_ssl = uri.scheme === "https"
+        request = Net::HTTP::Post.new('/')
+        request.basic_auth(uri.user, uri.password)
+        request.content_type = 'application/json'
+        request.body = data.to_json
+        response = http.request(request)
+        body = response.body
+        response = JSON.parse(body.gsub(/\\u([\da-fA-F]{4})/) { [$1].pack('H*').unpack('n*').pack('U*').encode('ISO-8859-1').force_encoding('UTF-8') })
+        raise response['error'].to_s if response['error']
+        response['result']
       end
       
       # Call CLI command on Ruby-like method names.
