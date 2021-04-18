b37d1f9413cef091145d40aace89c9eebe1b7b06
azuchi <azuchi@chaintope.com>
2021-04-18 16:37:07 +0900

    Split taproot spec to run parallel

---
+++ spec/bitcoin/taproot/script_assets_6_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+require 'bitcoin/taproot/taproot_spec_helper'
+
+RSpec.describe 'script_assets_test_6' do
+
+  let(:spec_path) { fixture_path('taproot/script_assets_test_6.json') }
+
+  it 'should be pass.' do
+    test_script_assets(spec_path)
+  end
+end
