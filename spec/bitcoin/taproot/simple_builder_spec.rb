9abb15b3391acac1f85587ab21e28ce353453b7b
azuchi <azuchi@chaintope.com>
2021-06-28 11:29:58 +0900

    Add spec for Bitcoin::Taproot::SimpleBuilder#build

--- spec/bitcoin/taproot/simple_builder_spec.rb
+++ spec/bitcoin/taproot/simple_builder_spec.rb
@@ -37,6 +37,12 @@ RSpec.describe Bitcoin::Taproot::SimpleBuilder, network: :mainnet do
 
     context 'hash scripts' do
       it 'should generate P2TR script' do
+        # tree leaves tree.
+        #       N0
+        #    /     \
+        #   N1      C
+        #  /  \
+        # A    B
         key1 = 'c6047f9441ed7d6d3045406e95c07cd85c778e4b8cef3ca7abac09b95c709ee5'
         key2 = 'f9308a019258c31049344f85f89d5229b531c845836f99b08601f113bce036f9'
         key3 = '31fe7061656bea2a36aa60a2f7ef940578049273746935d296426dc0afd86b68'
@@ -44,8 +50,44 @@ RSpec.describe Bitcoin::Taproot::SimpleBuilder, network: :mainnet do
         script2 = Bitcoin::Script.new << key2 << OP_CHECKSIG
         script3 = Bitcoin::Script.new << key3 << OP_CHECKSIG
         builder = Bitcoin::Taproot::SimpleBuilder.new(internal_key,script1, script2, script3)
-        p2tr = builder.build
-        expect(p2tr.to_addr).to eq('bc1pkysykpr4e9alyu7wthrtmp2km6sylrjlz83qzrsjkxhdaazyfrusyk2pwg')
+        expect(builder.build.to_addr).to eq('bc1pkysykpr4e9alyu7wthrtmp2km6sylrjlz83qzrsjkxhdaazyfrusyk2pwg')
+
+        # four leaves tree.
+        #       N0
+        #    /     \
+        #   N1      N2
+        #  /  \    /  \
+        # A    B  C    D
+        key4 = 'a016430f275c30cb15f399aa807cc9bde6b2c4c80c84be3bb27912089c18e363'
+        script4 = Bitcoin::Script.new << key4 << OP_CHECKSIG
+        builder << script4
+        expect(builder.build.to_addr).to eq('bc1pwr9amrwnxplrxdealu7h9rnfxusrdu8266ec83jpch3khjys9t9scpnpv6')
+
+        # five leaves tree.
+        #           N0
+        #        /     \
+        #       N1      E
+        #    /     \
+        #   N2      N3
+        #  /  \    /  \
+        # A    B  C    D
+        key5 = 'b256afd27b26b0db101fd4a3d99afdd876dd2aaa5be967198882476bf425c301'
+        script5 = Bitcoin::Script.new << key5 << OP_CHECKSIG
+        builder << script5
+        expect(builder.build.to_addr).to eq('bc1pmxmu5slfv0zm3dsju74djrhkhd75qwpwht688vn0kw5f9j4z855sdx8c39')
+
+        # six leaves tree.
+        #            N0
+        #        /        \
+        #       N1         N2
+        #    /     \      /  \
+        #   N3      N4   E    F
+        #  /  \    /  \
+        # A    B  C    D
+        key6 = '0e5ba1cfed1fe76ff81558731b7279ed23ddd95ce0fd67adc94584e80abbe987'
+        script6 = Bitcoin::Script.new << key6 << OP_CHECKSIG
+        builder << script6
+        expect(builder.build.to_addr).to eq('bc1peyxqpa4c4uzg9jzt7q92c94sk7kmj2uzhh2phuv8q8tkjrw6y67qf0u6mu')
       end
     end
   end
