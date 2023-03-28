47bba9c5ce656fed97c37197e07ce8fe01b7b26a
azuchi <azuchi@chaintope.com>
2023-03-28 11:41:41 +0900

    Implement BIP-322 message sign

--- lib/bitcoin/message_sign.rb
+++ lib/bitcoin/message_sign.rb
@@ -6,14 +6,42 @@ module Bitcoin
 
     module_function
 
+    FORMAT_LEGACY = :legacy
+    FORMAT_SIMPLE = :simple
+    FORMAT_FULL = :full
+
     # Sign a message.
-    # @param [Bitcoin::Key] key Private key to sign with.
-    # @param [String] message The message to sign.
+    # @param [Bitcoin::Key] key Private key to sign.
+    # @param [String] message The message to be signed.
+    # @param [String] address An address of the key used for signing (required for full or simple format).
+    # @param [String] format Format of signature data. Default is +FORMAT_LEGACY+.
+    # @param [String] prefix (Optional) Prefix used in legacy format.
     # @return [String] Signature, base64 encoded.
-    def sign_message(key, message, prefix: Bitcoin.chain_params.message_magic)
-      digest = message_hash(message, prefix: prefix)
-      compact_sig = key.sign_compact(digest)
-      Base64.strict_encode64(compact_sig)
+    def sign_message(key, message, prefix: Bitcoin.chain_params.message_magic, format: FORMAT_LEGACY, address: nil)
+      validate_format!(format)
+      digest = message_hash(message, prefix: prefix, legacy: format == FORMAT_LEGACY)
+      sig = case format
+            when FORMAT_LEGACY
+              key.sign_compact(digest)
+            else
+              validate_address!(address)
+              addr = Bitcoin::Script.parse_from_addr(address)
+              sig_ver, algo = if addr.p2wpkh?
+                                [:witness_v0, :ecdsa]
+                              elsif addr.p2tr?
+                                [:taproot, :schnorr]
+                              else
+                                raise ArgumentError "#{address} dose not supported."
+                              end
+              tx = to_sign_tx(digest, address)
+              prev_out = Bitcoin::TxOut.new(script_pubkey: addr)
+              sighash = tx.sighash_for_input(0, addr, sig_version: sig_ver, amount: 0, prevouts: [prev_out])
+              sig = key.sign(sighash, algo: algo) + [Bitcoin::SIGHASH_TYPE[:all]].pack('C')
+              tx.in[0].script_witness.stack << sig
+              tx.in[0].script_witness.stack << key.pubkey.htb
+              format == FORMAT_SIMPLE ? tx.in[0].script_witness.to_payload : tx.to_payload
+            end
+      Base64.strict_encode64(sig)
     end
 
     # Verify a signed message.
@@ -23,25 +51,72 @@ module Bitcoin
     # @return [Boolean] Verification result.
     def verify_message(address, signature, message, prefix: Bitcoin.chain_params.message_magic)
       validate_address!(address)
-      sig = Base64.decode64(signature)
-      raise ArgumentError, 'Invalid signature length' unless sig.bytesize == Bitcoin::Key::COMPACT_SIGNATURE_SIZE
-      digest = message_hash(message, prefix: prefix)
-      pubkey = Bitcoin::Key.recover_compact(digest, sig)
-      return false unless pubkey
-      pubkey.to_p2pkh == address
+      begin
+        sig = Base64.strict_decode64(signature)
+      rescue ArgumentError
+        raise ArgumentError, 'Invalid signature'
+      end
+      begin
+        # Legacy verification
+        pubkey = Bitcoin::Key.recover_compact(message_hash(message, prefix: prefix, legacy: true), sig)
+        return false unless pubkey
+        pubkey.to_p2pkh == address
+      rescue ArgumentError
+        # BIP322 verification
+        tx = to_sign_tx(message_hash(message, prefix: prefix, legacy: false), address)
+        tx.in[0].script_witness = Bitcoin::ScriptWitness.parse_from_payload(sig)
+        script_pubkey = Bitcoin::Script.parse_from_addr(address)
+        tx_out = Bitcoin::TxOut.new(script_pubkey: script_pubkey)
+        interpreter = Bitcoin::ScriptInterpreter.new(checker: Bitcoin::TxChecker.new(tx: tx, input_index: 0, prevouts: [tx_out]))
+        interpreter.verify_script(Bitcoin::Script.new, script_pubkey, tx.in[0].script_witness)
+      end
     end
 
     # Hashes a message for signing and verification.
-    def message_hash(message, prefix: Bitcoin.chain_params.message_magic)
-      Bitcoin.double_sha256(Bitcoin.pack_var_string(prefix) << Bitcoin.pack_var_string(message))
+    def message_hash(message, prefix: Bitcoin.chain_params.message_magic, legacy: true)
+      if legacy
+        Bitcoin.double_sha256(Bitcoin.pack_var_string(prefix) << Bitcoin.pack_var_string(message))
+      else
+        Bitcoin.tagged_hash('BIP0322-signed-message', message)
+      end
     end
 
     def validate_address!(address)
       raise ArgumentError, 'Invalid address' unless Bitcoin.valid_address?(address)
       script = Bitcoin::Script.parse_from_addr(address)
-      raise ArgumentError, 'Address has no key' unless script.p2pkh?
+      raise ArgumentError, 'This address unsupported' if script.p2sh? || script.p2wsh?
+    end
+
+    def validate_format!(format)
+      unless [FORMAT_LEGACY, FORMAT_FULL, FORMAT_SIMPLE].include?(format)
+        raise ArgumentError "Invalid format specified."
+      end
+    end
+
+    def to_spend_tx(digest, addr)
+      validate_address!(addr)
+      message_challenge = Bitcoin::Script.parse_from_addr(addr)
+      tx = Bitcoin::Tx.new
+      tx.version = 0
+      tx.lock_time = 0
+      prev_out = Bitcoin::OutPoint.create_coinbase_outpoint
+      script_sig = Bitcoin::Script.new << Bitcoin::Opcodes::OP_0 << digest
+      tx.in << Bitcoin::TxIn.new(out_point: prev_out, sequence: 0, script_sig: script_sig)
+      tx.out << Bitcoin::TxOut.new(script_pubkey: message_challenge)
+      tx
+    end
+
+    def to_sign_tx(digest, addr)
+      tx = Bitcoin::Tx.new
+      tx.version = 0
+      tx.lock_time = 0
+      prev_out = Bitcoin::OutPoint.from_txid(to_spend_tx(digest, addr).txid, 0)
+      tx.in << Bitcoin::TxIn.new(out_point: prev_out, sequence: 0)
+      tx.out << Bitcoin::TxOut.new(script_pubkey: Bitcoin::Script.new << Bitcoin::Opcodes::OP_RETURN)
+      tx
     end
 
     private_class_method :validate_address!
+    private_class_method :validate_format!
   end
 end
--- spec/bitcoin/message_sign_spec.rb
+++ spec/bitcoin/message_sign_spec.rb
@@ -59,6 +59,48 @@ RSpec.describe Bitcoin::MessageSign, network: :mainnet do
     end
   end
 
+  describe "BIP322 Test Vector", network: :mainnet do
+    it do
+      # Message hashing
+      digest1 = described_class.message_hash('', legacy: false )
+      digest2 = described_class.message_hash('Hello World', legacy: false)
+      expect(digest1.bth).to eq('c90c269c4f8fcbe6880f72a721ddfbf1914268a794cbb21cfafee13770ae19f1')
+      expect(digest2.bth).to eq('f0eb03b1a75ac6d9847f55c624a99169b5dccba2a31f5b23bea77ba270de0a7a')
+
+      # Message signing
+      private_key = Bitcoin::Key.from_wif('L3VFeEujGtevx9w18HD1fhRbCH67Az2dpCymeRE1SoPK6XQtaN2k')
+      addr = private_key.to_p2wpkh
+      expect(addr).to eq("bc1q9vza2e8x573nczrlzms0wvx3gsqjx7vavgkx0l")
+      sig1 = described_class.sign_message(
+        private_key,
+        '',
+        format: described_class::FORMAT_SIMPLE,
+        address: addr)
+      expect(sig1).to eq('AkcwRAIgM2gBAQqvZX15ZiysmKmQpDrG83avLIT492QBzLnQIxYCIBaTpOaD20qRlEylyxFSeEA2ba9YOixpX8z46TSDtS40ASECx/EgAxlkQpQ9hYjgGu6EBCPMVPwVIVJqO4XCsMvViHI=')
+      sig2 = described_class.sign_message(
+        private_key,
+        'Hello World',
+        format: described_class::FORMAT_SIMPLE,
+        address: addr)
+      expect(sig2).to eq('AkcwRAIgZRfIY3p7/DoVTty6YZbWS71bc5Vct9p9Fia83eRmw2QCICK/ENGfwLtptFluMGs2KsqoNSk89pO7F29zJLUx9a/sASECx/EgAxlkQpQ9hYjgGu6EBCPMVPwVIVJqO4XCsMvViHI=')
+
+      # Transaction hash
+      to_spend1 = described_class.to_spend_tx(digest1, addr)
+      expect(to_spend1.txid).to eq('c5680aa69bb8d860bf82d4e9cd3504b55dde018de765a91bb566283c545a99a7')
+      to_spend2 = described_class.to_spend_tx(digest2, addr)
+      expect(to_spend2.txid).to eq('b79d196740ad5217771c1098fc4a4b51e0535c32236c71f1ea4d61a2d603352b')
+
+      to_sign1 = described_class.to_sign_tx(digest1, addr)
+      expect(to_sign1.txid).to eq('1e9654e951a5ba44c8604c4de6c67fd78a27e81dcadcfe1edf638ba3aaebaed6')
+      to_sign2 = described_class.to_sign_tx(digest2, addr)
+      expect(to_sign2.txid).to eq('88737ae86f2077145f93cc4b153ae9a1cb8d56afa511988c149c5c8c9d93bddf')
+
+      # Verify signature
+      expect(described_class.verify_message(addr, sig1, '')).to be true
+      expect(described_class.verify_message(addr, sig2, 'Hello World')).to be true
+    end
+  end
+
   def prefix(network)
     fixtures['networks'][network]
   end
@@ -107,13 +149,13 @@ RSpec.describe Bitcoin::MessageSign, network: :mainnet do
                                                "message too")}.to raise_error(ArgumentError, 'Invalid address')
     expect{Bitcoin::MessageSign.verify_message("3B5fQsEXEaV8v6U3ejYc8XaKXAkyQj2MjV",
                                                "signature should be irrelevant",
-                                               "message too")}.to raise_error(ArgumentError, 'Address has no key')
+                                               "message too")}.to raise_error(ArgumentError, 'This address unsupported')
     expect{Bitcoin::MessageSign.verify_message("1KqbBpLy5FARmTPD4VZnDDpYjkUvkr82Pm",
                                                "invalid signature, not in base64 encoding",
-                                               "message should be irrelevant")}.to raise_error(ArgumentError, 'Invalid signature length')
-    expect{Bitcoin::MessageSign.verify_message("1KqbBpLy5FARmTPD4VZnDDpYjkUvkr82Pm",
+                                               "message should be irrelevant")}.to raise_error(ArgumentError, 'Invalid signature')
+    expect(Bitcoin::MessageSign.verify_message("1KqbBpLy5FARmTPD4VZnDDpYjkUvkr82Pm",
                                                "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=",
-                                               "message should be irrelevant")}.to raise_error(ArgumentError, 'Invalid signature parameter')
+                                               "message should be irrelevant")).to be false
     expect(Bitcoin::MessageSign.verify_message("15CRxFdyRpGZLW9w8HnHvVduizdL5jKNbs",
                                                "IPojfrX2dfPnH26UegfbGQQLrdK844DlHq5157/P6h57WyuS/Qsl+h/WSVGDF4MUi4rWSswW38oimDYfNNUBUOk=",
                                                "I never signed this")).to be false
