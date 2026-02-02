420b553e1b989f7f639a3a77529795df6fa32adb
azuchi <azuchi@chaintope.com>
2026-02-02 09:59:13 +0900

    Remove redundant data

--- spec/bitcoin/message/wtixd_relay_spec.rb
+++ spec/bitcoin/message/wtixd_relay_spec.rb
@@ -1,11 +1,12 @@
 require 'spec_helper'
 RSpec.describe Bitcoin::Message::WTXIDRelay, network: :mainnet do
+  let(:pkt) { 'f9beb4d9777478696472656c61790000000000005df6e0e2' }
 
   it 'should generate wtxid message' do
     wtxid_relay = Bitcoin::Message::WTXIDRelay.new
-    expect(wtxid_relay.to_pkt.bth).to eq('f9beb4d9777478696472656c61790000000000005df6e0e2')
+    expect(wtxid_relay.to_pkt.bth).to eq(pkt)
 
-    wtxid_relay = Bitcoin::Message::Base.from_pkt('f9beb4d9777478696472656c61790000000000005df6e0e2'.htb)
+    wtxid_relay = Bitcoin::Message::Base.from_pkt(pkt.htb)
     expect(wtxid_relay).to be_a(Bitcoin::Message::WTXIDRelay)
   end
 
